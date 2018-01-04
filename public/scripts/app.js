c389ab13c590899b8f14890ae9e3a9e136e6041e
Imre Bartis <imre.bartis@siili.com>
2018-01-04 12:09:59 +0200

    conditional rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,54 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var user = {
-    name: 'Jay',
-    age: '66',
-    place: 'Betelgeuse'
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
 
+var title = app.title,
+    subtitle = app.subtitle,
+    options = app.options;
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    title
+  ),
+  subtitle && React.createElement(
+    'p',
+    null,
+    subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var user = {
+  name: 'Jay',
+  age: '17',
+  place: 'Betelgeuse'
 };
 
 var name = user.name,
@@ -15,26 +58,34 @@ var name = user.name,
     place = user.place;
 
 
-var template = React.createElement(
-    'div',
+function getLocation(something) {
+  if (something) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      place
+    );
+  }
+
+  // return 'unknown'
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
     null,
-    React.createElement(
-        'h1',
-        null,
-        name.toUpperCase()
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age: ',
-        age
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        place
-    )
+    user.name ? user.name.toUpperCase() : 'Anonymous'
+  ),
+  age && age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    age
+  ),
+  getLocation(place)
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,20 +1,46 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
+const app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
+  };
+
+  const {title, subtitle, options} = app;
+  const template = (
+    <div>
+      <h1>{title}</h1>
+      {subtitle && <p>{subtitle}</p>}
+      <p>{options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+    </div>
+  );
+
 const user = {
     name: 'Jay',
-    age: '66',
+    age: '17',
     place: 'Betelgeuse'
-
 }
 
 const {name, age, place} = user
 
-const template = (
+function getLocation(something) {
+    if (something) {
+      return <p>Location: {place}</p>
+    }
+
+    // return 'unknown'
+  }
+
+const templateTwo = (
     <div>
-        <h1>{name.toUpperCase()}</h1>
-        <p>Age: {age}</p>
-        <p>Location: {place}</p>
+        <h1>{user.name ? user.name.toUpperCase() : 'Anonymous'}</h1>
+        {(age && age >= 18) && <p>Age: {age}</p>}
+        {getLocation(place)}
     </div>
 );
 const appRoot = document.getElementById('app');
