d31e6eedac752f175205d849ee7b5945aefccdc5
Imre Bartis <imre.bartis@siili.com>
2018-01-04 11:09:59 +0200

    jsx expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,38 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
+var user = {
+    name: 'Jay',
+    age: '66',
+    place: 'Betelgeuse'
+
+};
+
+var name = user.name,
+    age = user.age,
+    place = user.place;
+
+
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        name.toUpperCase()
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        place
+    )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+const user = {
+    name: 'Jay',
+    age: '66',
+    place: 'Betelgeuse'
+
+}
+
+const {name, age, place} = user
+
+const template = (
+    <div>
+        <h1>{name.toUpperCase()}</h1>
+        <p>Age: {age}</p>
+        <p>Location: {place}</p>
+    </div>
+);
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
