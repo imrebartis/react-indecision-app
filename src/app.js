17e8fc166f99597c3acec1fcdd5585d14c964567
Imre Bartis <imre.bartis@siili.com>
2018-01-11 14:18:35 +0200

    set up babel with webpack

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const template = React.createElement('p', {}, 'testing 123');
+const template = <p> This is jsx </p>;
 ReactDOM.render(template, document.getElementById('app'));
 
 // import validator from 'validator';
