e1876de0213db11cbad2dfa6d2e1b3676b016ff2
Imre Bartis <imre.bartis@siili.com>
2018-01-11 13:46:09 +0200

    import npm modules

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-import subtract, { square, add } from './utils.js';
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('app.js is running');
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 22));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
+
+// import validator from 'validator';
+
+// console.log(validator.isEmail('test@gmail.com'))
